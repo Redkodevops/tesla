a5ef63b573294c2022e2ace11e8406d656ad6dff
ec2-user <ekodevops2815@yahoo.com>
2022-05-17 15:02:35 -0500

    deployment modified

--- tesla.sh
+++ tesla.sh
@@ -1,3 +1,5 @@
 echo  -e "The lord is my shepherd\nI shall not want\nHe make me lie in green pastures."  
 He restores my soul
 He leads me beside the still waters
+For His name sake
+He leads me on the path of righteousness
