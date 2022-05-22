23f57ff923eecf1d5f1ffef66096b2f995d0108a
ec2-user <ekodevops2815@yahoo.com>
2022-05-22 13:00:23 -0500

    new files

--- tesla.sh
+++ tesla.sh
@@ -3,3 +3,6 @@ He restores my soul
 He leads me beside the still waters
 For His name sake
 He leads me on the path of righteousness
+Even though i walk through the valley of shadow of death
+I wil fear no evil
+Cos though are with me
